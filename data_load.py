806443a7028ba6ba98f32db79c63b62b8eec6949
Facu <facu@marte>
2018-05-23 19:30:33 -0300

    I add some auxiliar functions and the data_load

---
+++ data_load.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy as sp
+import random as rd
+import sklearn.cluster as cl
+import sklearn.metrics as mt
+from sklearn import datasets
+import sklearn.datasets as data
+import matplotlib.pyplot as plt
+from sklearn import manifold
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from sklearn import decomposition
+import heapq
+import itertools
+import pickle
+from datetime import datetime
+import itertools as itl
+import os
+
+plt.rc('text', usetex=True)       # Notacion LaTeX
+plt.rc('font', family='serif')
